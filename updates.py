291c56bccb76de312849c95b575f8216f777aa94
Kudryavtsev Kirill <kudryavtsev@nppsatek.ru>
2019-09-15 12:02:47 +0300

    fix: logging

--- updates.py
+++ updates.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import requests
 from loguru import logger as log
+
+# setup logger
+log.add("moonwalk_update_{time}.log", retention="1 week", compression="zip", enqueue=True, serialize=True) 
+
 from src.moonwalk.api import MoonwalkAPI
 from src.transform import CreateNew, Update
 
